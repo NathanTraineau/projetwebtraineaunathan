3c5df46610fcbe93ed28a8b5910b6b4c52d3fede
White Jacques <32514062+NathanTraineau@users.noreply.github.com>
2019-04-22 22:32:28 +0200

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -151,7 +151,7 @@ class User extends CI_Controller {
         )
 );
         $msg = htmlspecialchars($_POST['username']);
-           $this->form_validation->set_message('username_callable', 'Ma grosse bite, répond à l enigme');
+           $this->form_validation->set_message('username_callable', 'Ma grosse bite ce serait aussi simple. Il y a une enigme, \n Username :" Le pseudo de celui qui aurait du être respo com" \n mdp :" le copié collé de son commentaire rageux qui est resté dans les anales');
 
  			if ($this->form_validation->run() == FALSE)
                 {
