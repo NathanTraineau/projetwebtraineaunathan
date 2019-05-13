c4cb374dcf9776244808c9e0607e3172775c5e29
White Jacques <32514062+NathanTraineau@users.noreply.github.com>
2019-05-13 13:23:38 +0200

    Update login.php

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -27,7 +27,7 @@
               <div class="text-center"><input class="btn btn-primary btn-success btn-block" type="submit" value="login" /></div>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="<?php echo site_url("user/registration")?>">Register an Account</a>
+          <a class="d-block small mt-3" href="<?php echo site_url("user/test")?>">Register an Account</a>
         </div>
       </div>
     </div>
